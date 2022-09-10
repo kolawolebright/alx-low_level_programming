1de5f2a14cfb2febb5a58b6902e9e0d0f03bcdcf
Kolawole Odedele <odedelebright@gmail.com>
2022-09-10 14:33:39 -0700

    Program to print on the screen

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-
-int main(void)
-
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Succes)
+ * **/
+int main (void)
 {
-	puts("programming is like building a multilingual puzzle");
-
-	return 0;
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
