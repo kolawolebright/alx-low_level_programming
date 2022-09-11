191d8ea612309f919e671e8f57e258f1e0329e10
Kolawole Odedele <odedelebright@gmail.com>
2022-09-11 13:45:15 -0700

    A program that prints on the screen

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.hi>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{write(1, "and that piece of art is useful\" - Dora Korpa, 2015-19-19\n", 59);
+	return (1);
+}
