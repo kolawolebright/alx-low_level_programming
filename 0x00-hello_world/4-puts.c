f19a40ceb9207c3c461764271bbc440a4b76cb99
Kolawole Odedele <odedelebright@gmail.com>
2022-09-11 00:14:02 -0700

    Program that prints on the phone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include<stdio.h>	
-int main (void)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ * **/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	Return (0);
+}
